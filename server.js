7a41cc2891a7e396d28932bc3c2ed0e1f99083f0
gkshi <gkshi@ya.ru>
2021-06-01 21:28:51 +0300

    [EDIT] server static files handling

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (req, res) => {
+app.use(express.static(__dirname + '/build'))
+
+app.get('*', (req, res) => {
   res.sendFile('./build/index.html', { root: __dirname })
+  // res.send('ok')
 })
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.APP_PORT, () => {
   console.log('server listening at', process.env.APP_PORT)
 })
