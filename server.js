a300e0c19fd45f4f54ef3d6bbf1f6d5d54d4f346
gkshi <gkshi@ya.ru>
2021-06-03 23:39:20 +0300

    [REFACTOR] env files refactoring

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 require('dotenv').config({
-  path: process.env.NODE_ENV === 'development' ? '.env.local' : '.env.prod'
+  path: process.env.NODE_ENV === 'development' ? '.env.development' : '.env.production'
 })
 
 const express = require('express')
