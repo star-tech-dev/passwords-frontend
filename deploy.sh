94b89f4d16578e6e95fe10538893291e76c3e644
George Shinkarev <gkshi@ya.ru>
2022-04-25 01:22:49 +0300

    [EDIT] deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 cd /var/www/star-tech-dev/passwords-frontend;
 pm2 delete passwords-frontend;
 npm i;
-npm run build;
+# not enough server memory to run
+# npm run build;
 pm2 start serve.sh --name passwords-frontend -l;
